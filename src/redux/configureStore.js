171514b130ff176550d6b7c7fa8fd9692f28e286
Burak Fircasiguzel <burakfguzel@gmail.com>
2022-02-15 01:02:33 +0300

    72 / LocalStorage Encryption

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,32 +1,35 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
+import SecureLS from 'secure-ls';
 
-  const configureStore = () => {
+const secureLs = new SecureLS();
 
-    const hoaxAuth = localStorage.getItem('hoax-auth');
+const getStateFromStorage = () => {
+    const hoaxAuth = secureLs.get('hoax-auth');
     let stateInLocalStorage = {
-        isLoggedIn : false,
+        isLoggedIn: false,
         username: undefined,
         displayName: undefined,
         image: undefined,
         password: undefined
-      }
-    if(hoaxAuth){
-        try{
-            stateInLocalStorage =  JSON.parse(hoaxAuth);
-        }catch(error){
-
-        }
-        
     }
+    if (hoaxAuth) {
+        return hoaxAuth;
+    }
+    return stateInLocalStorage;
+};
 
-     const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-     
-     store.subscribe(() => {
-        localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
-     });
+const updateStateInStorage = newState => {
+    secureLs.set('hoax-auth', newState);
+};
 
-     return store;
-  }
+const configureStore = () => {
+    const store = createStore(authReducer, getStateFromStorage(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-  export default configureStore;
+    store.subscribe(() => {
+        updateStateInStorage(store.getState());
+    });
+    return store;
+};
+
+export default configureStore;
